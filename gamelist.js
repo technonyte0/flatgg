158528ffb71a43b82814dbcac1932cc044d033f1
technonyte0 <144625553+technonyte0@users.noreply.github.com>
2023-09-10 20:36:03 -0400

    Create gamelist.js

---
+++ gamelist.js
@@ -0,0 +1,5 @@
+const games = [
+  { title: "Minecraft", description: "The classic sandbox game.", url: "https://ubg100.github.io/games/eaglercraft/index.html" },
+  { title: "1v1.lol", description: "A Fortnite-ish clone.", url: "https://1v1.lol/" },
+  { title: "Krunker", description: "Krunker?", url: "https://krunker.io" },
+];
